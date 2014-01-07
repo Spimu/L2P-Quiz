68fc7ac007a43fe6c36d72fb597235c608c47156
Claude Bemtgen <claude.bemtgen@rwth-aachen.de>
2014-01-07 12:02:02 +0100

    Initial Commit

---
+++ L2P-QuizTests/L2P_QuizTests.m
@@ -0,0 +1,34 @@
+//
+//  L2P_QuizTests.m
+//  L2P-QuizTests
+//
+//  Created by Claude Bemtgen on 1/7/14.
+//  Copyright (c) 2014 RWTHi10. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface L2P_QuizTests : XCTestCase
+
+@end
+
+@implementation L2P_QuizTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
