e54dff6778e2b5789a4645b18ee9e2c2f42e63cb
Marty Pye <pye.marty@gmail.com>
2014-01-17 13:43:44 +0100

    Added the dots

--- L2P-Quiz/AppDelegate.m
+++ L2P-Quiz/AppDelegate.m
@@ -18,6 +18,11 @@
 {
     [[UIApplication sharedApplication] setMinimumBackgroundFetchInterval:UIApplicationBackgroundFetchIntervalMinimum];
     
+    UIPageControl *pageControl = [UIPageControl appearance];
+    pageControl.pageIndicatorTintColor = [UIColor lightGrayColor];
+    pageControl.currentPageIndicatorTintColor = [UIColor blackColor];
+    pageControl.backgroundColor = [UIColor whiteColor];
+    
     return YES;
 }
 
