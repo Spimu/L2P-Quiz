c7666074df09b7a5cc0e64f9f2df5ce1ea26902d
Marty Pye <pye.marty@gmail.com>
2014-01-17 15:03:21 +0100

    Example Plot added. To be filled with real data.

--- L2P-Quiz/StatisticsPageContentViewController.h
+++ L2P-Quiz/StatisticsPageContentViewController.h
@@ -7,6 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CPDConstants.h"
+#import "CPDStockPriceStore.h"
+
 
 @interface StatisticsPageContentViewController : UIViewController <CPTBarPlotDataSource, CPTBarPlotDelegate>
 
